22348c7b1ae9dddd94a70d34f0316ae197396f29
Leslie Figueroa <lesliek.x1999@gmail.com>
2026-03-05 21:20:34 -0300

    se corrige variable de la imagen

--- likeme-frontend/src/components/Post.jsx
+++ likeme-frontend/src/components/Post.jsx
@@ -1,15 +1,12 @@
 function Post({
-  post: { id, titulo, img, descripcion, likes },
+  post: { id, titulo, url, descripcion, likes },
   like,
   eliminarPost,
 }) {
   return (
     <div className="card col-12 col-sm-4 d-inline mx-0 px-3">
       <div className="card-body  p-0">
-        <img
-          className="card-img-top "
-          src={img}
-        />
+        <img className="card-img-top " src={url} />
         <div className="p-3">
           <h4 className="card-title">{titulo}</h4>
           <p className="card-text">{descripcion}</p>
@@ -23,10 +20,7 @@ function Post({
               ></i>
               <span className="ms-1">{likes}</span>
             </div>
-            <i
-              onClick={() => eliminarPost(id)}
-              className="fa-solid fa-x"
-            ></i>
+            <i onClick={() => eliminarPost(id)} className="fa-solid fa-x"></i>
           </div>
         </div>
       </div>
